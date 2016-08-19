93019fc36887d0f8b45c2ebafda2864743693068
Alexander <strumaticjoker2@gmail.com>
2016-08-19 10:47:23 +0200

    Update logout.php
    Remove .php extension

--- admin/logout.php
+++ admin/logout.php
@@ -1,4 +1,11 @@
 <?php 
+
+  // Remove .php extension
+	if (strpos($_SERVER['REQUEST_URI'],".php") !== false) {
+		$newUri = str_replace(".php","",$_SERVER['REQUEST_URI']);
+		header('Location: '.$newUri);
+	}
+
 session_start();
 session_destroy();
 header("Location: ./login.php");
